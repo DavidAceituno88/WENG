e58a8bb2c4c08fc504a4813c44e4b11232b51f6e
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2024-12-17 09:08:56 -0800

    Add files via upload

---
+++ products/expandable.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.expandable-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
